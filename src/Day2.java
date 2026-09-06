0c5147a275621093e06e5f736c5ab0fd9fbb3a66
Kavya <kavyashreebaindoor@gmail.com>
2026-09-06 18:54:14 +0530

    added ne code

--- src/Day2.java
+++ src/Day2.java
@@ -1,6 +1,8 @@
 public class Day2 { public static void main(String[] args) {
 
-    System.out.println("Learning Git Day 2");
+    System.out.println("Learning the Git Day 2");
+
+    System.out.println("hello");
 
 }
 }
